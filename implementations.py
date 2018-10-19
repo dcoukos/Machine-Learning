3c3012ed668d40e3f0d68be6a572ae1bca6e8818
Dimitri Coukos <dimitri.coukos@epfl.ch>
2018-10-19 09:48:08 +0200

    Start filling out implementations.py

---
+++ implementations.py
@@ -0,0 +1,38 @@
+import numpy as np
+"File containing helper functions developed during course exercises"
+
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    '''
+    Linear regression using gradient descent
+    '''
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    '''
+    Linear regression using stochastic gradient descent
+    '''
+
+
+def least_squares(y, tx):
+    '''
+    Least squares regression using normal equations
+    '''
+
+
+def ridge_regression(y, tx, lambda_):
+    '''
+    Ridge regression using normal equations
+    '''
+
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    '''
+    Logistic regression using gradient descent or SGD
+    '''
+
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    '''
+    Regularized logistic regression using gradient descent or SGD
+    '''
